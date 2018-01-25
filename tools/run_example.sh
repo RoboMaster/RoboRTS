be48d74f92ff1df8741bb051f127219b3cc2969f
noah003 <bit_gyn@163.com>
2018-01-25 14:58:01 +0800

    Update tools/run_example.sh, fix issue #1 and #3

--- tools/run_example.sh
+++ tools/run_example.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 echo "RoboRTS v0.5"
-#set -o errexit 
+
+function ProcessDetection() {
+    ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
+    result=$(echo $ps_out | grep "$1")
+    if [[ "$result" != "" ]];then
+        return 1
+    else
+        return 0
+    fi
+}
 
 gnome-terminal --window -e 'bash -c "roslaunch ${ROBORTS_PATH}/tools/stage/simulator.launch;exec bash"' \
-sleep 1
+ProcessDetection simulator.launch
+rst=$?
+while [ "$rst" = "0" ]; do
+    ProcessDetection simulator.launch
+    rst=$?
+    sleep 1
+done
+
 gnome-terminal --window -e 'bash -c "cd ${ROBORTS_PATH};./build/modules/perception/localization/localization_node;exec bash"' \
-sleep 1
 gnome-terminal --window -e 'bash -c "cd ${ROBORTS_PATH};./build/modules/planning/global_planner/global_planner_node;exec bash"' \
-sleep 1 
 gnome-terminal --window -e 'bash -c "cd ${ROBORTS_PATH};./build/modules/planning/local_planner/local_planner_node;exec bash"' \
-sleep 1
-gnome-terminal --window -e 'bash -c "cd ${ROBORTS_PATH};./build/modules/decision/decision_node;exec bash"' \
+gnome-terminal --window -e 'bash -c "cd ${ROBORTS_PATH};./build/modules/decision/decision_node;exec bash"'
