794016743af4f5f32d85b068bad3b3c6f333620c
charmyoung <charm0young@gmail.com>
2018-02-07 00:13:44 +0800

    Fix bugs in example

--- tools/run_example.sh
+++ tools/run_example.sh
@@ -11,19 +11,32 @@ function ProcessDetection() {
         return 0
     fi
 }
+MAP="rm"
+INPUT_MAP="$1"
+if [ ! -z "$INPUT_MAP" ]
+then
+  if [ -e "${ROBORTS_PATH}/tools/map/"$INPUT_MAP".yaml" ]
+  then 
+    MAP="$INPUT_MAP"
+  fi
+fi  
+echo "Open map ${MAP}"
+roslaunch ${ROBORTS_PATH}/tools/sample/example.launch map:=${MAP}&
+ProcessDetection example.launch
 
-gnome-terminal --window -e 'bash -c "roslaunch ${ROBORTS_PATH}/tools/stage/simulator_test.launch;exec bash"' \
-ProcessDetection simulator_test.launch
+
 rst=$?
 while [ "$rst" = "0" ]; do
-    ProcessDetection simulator_test.launch
+    ProcessDetection example.launch
     rst=$?
     sleep 1
 done
 
-gnome-terminal --window -e 'bash -c "cd ${ROBORTS_PATH};./build/modules/driver/serial/serial_com_node;exec bash"' \
-gnome-terminal --window -e 'bash -c "cd ${ROBORTS_PATH};./build/modules/stream/tf_tree/tf_tree;exec bash"' \
-gnome-terminal --window -e 'bash -c "cd ${ROBORTS_PATH};./build/modules/perception/localization/localization_node;exec bash"' \
-gnome-terminal --window -e 'bash -c "cd ${ROBORTS_PATH};./build/modules/planning/global_planner/global_planner_node;exec bash"' \
-gnome-terminal --window -e 'bash -c "cd ${ROBORTS_PATH};./build/modules/planning/local_planner/local_planner_node;exec bash"' \
-gnome-terminal --window -e 'bash -c "cd ${ROBORTS_PATH};./build/modules/decision/decision_node;exec bash"'
+cd ${ROBORTS_PATH}
+./build/modules/driver/serial/serial_com_node &
+./build/modules/stream/tf_tree/tf_tree &
+./build/modules/perception/localization/localization_node &
+./build/modules/planning/global_planner/global_planner_node &
+./build/modules/planning/local_planner/local_planner_node &
+./build/modules/decision/decision_node &
+echo "finish!"
